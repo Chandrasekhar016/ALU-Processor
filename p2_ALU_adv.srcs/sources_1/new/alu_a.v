0843b3316b30cc80287e20878e2389ec3619c31f
Chandrasekhar016 <kanikechandrashekhar@gmail.com>
2026-07-22 22:44:46 +0530

    pictures added

--- p2_ALU_adv.srcs/sources_1/new/alu_a.v
+++ p2_ALU_adv.srcs/sources_1/new/alu_a.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/12/2026 04:49:00 PM
-// Design Name: 
-// Module Name: alu_a
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module alu_a(
     input [7:0]a,
     input [7:0]b,
@@ -27,27 +5,21 @@ module alu_a(
     output reg [7:0]res,
     output reg carry,
     output reg zero,
-    output reg overflow
-    );
+    output reg overflow);
     always @(*) begin
-
     // Default values
     res   = 8'b00000000;
     carry    = 1'b0;
     overflow = 1'b0;
-
     case(opcode)
-    
     3'b000: begin
         {carry, res} = a + b;
         overflow = (~(a[7] ^ b[7])) & (a[7] ^ res[7]);
     end                                     //Addition
-    
     3'b001: begin
         {carry, res} = a - b;
         overflow = (a[7] ^ b[7]) & (a[7] ^ res[7]);
     end                                      //Subtraction
-    
     3'b010: res= a & b;                      //AND
     3'b011: res= a | b;                      //OR
     3'b100: res= a ^ b;                      //XOR
@@ -58,13 +30,11 @@ module alu_a(
     3'b111: begin {carry,res} = a - 1 ;
     overflow = a[7] & (~res[7]);
     end                                     //Decrement
-    
     default: begin
     res = 8'b0;
     carry = 0;
     overflow = 0;
     end
-    
     endcase
     end
  endmodule
